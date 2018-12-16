1dec900e81b95c1abc1fb684196b902f04d1f7e4
stakiran <stakiran@users.noreply.github.com>
2018-12-16 20:33:25 +0900

    first commit, but docs not yet.

---
+++ README.txt
@@ -0,0 +1,17 @@
+ 
+==================
+    BlackTimer
+==================
+
+■インストール方法
+
+■ライセンス
+MIT License
+
+■更新履歴
+・2018/12/16 v0.1.0
+  ・初版
+
+■作者
+stakiran:
+https://github.com/stakiran
---
+++ blacktimer.bat
@@ -0,0 +1,98 @@
+@echo off
+setlocal enabledelayedexpansion
+
+rem BlackTimer v0.1.0
+rem ================
+
+set waitmin=%1
+set msg=%*
+
+set selfdir_with_delim=%~dp0
+set datafoldername=.blacktimer
+set datafolderfull=%selfdir_with_delim%%datafoldername%
+set dataext=.txt
+
+set reremind_interval_sec=30
+
+if not exist %datafolderfull% (
+	echo 初回起動につき, データファイル保存先 "%datafolderfull%" を作成します.
+	mkdir %datafolderfull%
+)
+
+if "%waitmin%"=="" (
+	set /p waitmin="[Step 1/2] 何分後にリマインドしますか？ >>> "
+)
+
+if "%msg%"=="" (
+	set /p msg="[Step 2/2] リマインド時のメッセージは？(空欄可) >>> "
+)
+
+rem Re-check for 'set /p'
+if "%waitmin%"=="" (
+	set waitmin=0
+)
+if "%msg%"=="" (
+	set msg="時間になりました！"
+)
+
+set todaydate=%date%
+set todaydate_for_disp=%todaydate%
+set todaydate=%todaydate:/=%
+set todaydate=%todaydate:~0,8%
+set todaytime=%time%
+set todaytime_for_disp=%todaytime%
+set todaytime=%todaytime: =0%
+set todaytime=%todaytime::=%
+set todaytime=%todaytime:.=%
+set todaytime=%todaytime:~0,8%
+set todaydatetime=%todaydate%_%todaytime%
+
+set databasename=%todaydatetime%
+set datafilename=%databasename%%dataext%
+set datafilefull=%datafolderfull%\%datafilename%
+
+set /a waitsec=%waitmin%*60
+
+echo 【リマインド内容】 > %datafilefull%
+echo %msg% >> %datafilefull%
+echo.>> %datafilefull%
+echo ======== >> %datafilefull%
+echo.>> %datafilefull%
+echo Set from: %todaydate_for_disp% %todaytime_for_disp%.  >> %datafilefull%
+echo Wait sec: %waitmin% Minutes.  >> %datafilefull%
+
+title %waitmin%分後にリマインド
+timeout /T %waitsec% /nobreak
+start "" /max notepad "%datafilefull%"
+
+rem rereminding...
+rem ==============
+
+echo リリマインドを行います...
+echo リリマインド間隔: %reremind_interval_sec%[秒]
+
+set commandline=tasklist /fi "windowtitle eq %datafilename% - メモ帳" /nh
+set tasklist_result=
+set query=notepad.exe
+
+rem [ロジック解説]
+rem リマインドされたメモ帳ウィンドウの有無を, インターバル毎に, 調べる.
+rem 存在する = まだリマインドに気付いてない, とみなして再リマインドする.
+rem 
+rem 無限ループの実現には For 構文は使っていない.
+rem (理由) For 構文には goto や exit /b が動作しないという仕様があるため.
+
+:label_loopstart
+
+	timeout /t %reremind_interval_sec% /nobreak
+
+	for /f "usebackq delims=" %%a in (`%commandline%`) do set tasklist_result=%%a
+	echo !tasklist_result! | findstr %query% > nul
+	if errorlevel 1 goto label_loopend
+
+	start "" /max notepad "%datafilefull%"
+	goto label_loopstart
+
+:label_loopend
+
+exit /b
